2c7fd5b4149bf7a1bdbef6a145d32e8b0b1a0e9f
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2023-11-30 20:51:55 -0300

    project configuration

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+	console.log('');
+
+	return (
+		<></>
+	);
+};
+
+export default Navbar;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styles from './Home.styles';
+
+const Home = () => (
+	<Styles.Wrapper>
+    Olá, mundo!
+	</Styles.Wrapper>
+);
+
+export default Home;
