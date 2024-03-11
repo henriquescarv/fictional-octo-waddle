21c19ffc2f537ee4e1d94c4920a18351e7356f69
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2024-03-11 19:35:27 -0300

    add marveldex to projects lsit

--- src/data/projetcs.ts
+++ src/data/projetcs.ts
@@ -1,15 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import marveldexPreview from '../assets/images/projects/marveldex.png';
 import myPortfolioPreview from '../assets/images/projects/my_portfolio.png';
 import oldPortfolioPreview from '../assets/images/projects/old_portfolio.png';
 
 const projects = [
 	{
+		title: '#marveldex',
+		imageSrc: marveldexPreview,
+		imageAlt: 'marveldex screen preview',
+		description: {
+			br: 'Aplicação que consome uma poderosa API da Marvel. Exibe listas de personagens, quadrinhos e autores. ReactJS, Typescript, Styled Components e Redux.',
+			en: 'App consuming a powerful Marvel API. Displays lists of characters, comics, and authors, as well as details for each item. ReactJS, Typescript, Styled Components and Redux.',
+		},
+		projectUrl: 'https://github.com/henriquescarv/marveldex',
+		deployUrl: 'https://marveldex.vercel.app/',
+	},
+	{
 		title: '#my-portfolio',
 		imageSrc: myPortfolioPreview,
-		imageAlt: 'screen',
+		imageAlt: 'my portfolio screen preview',
 		description: {
-			br: 'Este portfólio que você está vendo. Engloba meus projetos mais relevantes e informações sobre minha carreira até aqui. Construído com ReactJS, Typescript e Styled Components',
+			br: 'Este portfólio que você está vendo. Engloba meus projetos mais relevantes e informações sobre minha carreira até aqui. Construído com ReactJS, Typescript e Styled Components.',
 			en: 'This portfolio you are viewing. Presents my most relevant projects and information about my career so far. Built with ReactJS, Typescript, and Styled Components.',
 		},
 		projectUrl: 'https://github.com/henriquescarv/my-portfolio',
@@ -18,9 +30,9 @@ const projects = [
 	{
 		title: '#old-portfolio',
 		imageSrc: oldPortfolioPreview,
-		imageAlt: 'screen',
+		imageAlt: 'my old portfolio screen preview',
 		description: {
-			br: 'Meu portfólio antigo. Criei em um momento onde estava querendo aprender conceitos a respeito do Next.JS, portanto, é estruturado com Next.JS, TypeScript e SASS',
+			br: 'Meu portfólio antigo. Criei em um momento onde estava querendo aprender conceitos a respeito do Next.JS, portanto, é estruturado com Next.JS, TypeScript e SASS.',
 			en: 'My old portfolio. I created it at a time when I was trying to learn concepts about Next.JS framework, so it was structured with Next.JS, TypeScript, and SASS.',
 		},
 		projectUrl: 'https://github.com/henriquescarv/old-portfolio',
